2179c5e8affb2ef0cbd6dd024e0e1473239efc2a
aayemaw <135789968+aayemaw@users.noreply.github.com>
2023-06-10 07:00:56 +0800

    Add API folder

---
+++ WebAPI/Controllers/ClientController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Entities;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {        
+        private readonly DataContext _context;
+        public ClientController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Client>>> Get()
+        {
+            return Ok(await _context.Clients.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> Get(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if(client == null)            
+                return BadRequest("Client not found.");
+           
+            return Ok(client);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Client>>> AddClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Clients.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<Client>>> UpdateClient(Client updateclient)
+        {
+            var sclient = await _context.Clients.FindAsync(updateclient.Id);
+
+            if (sclient == null)
+            {
+                return BadRequest("Client not found.");
+            }
+            else
+            {
+                sclient.Name = updateclient.Name;
+                sclient.Address = updateclient.Address;
+                sclient.JoinedDate = updateclient.JoinedDate;
+
+                await _context.SaveChangesAsync();
+
+                return Ok(await _context.Clients.ToListAsync());
+            }
+                
+        }
+
+        [HttpDelete("{id}")]        
+        public async Task<ActionResult<List<Client>>> DeleteClient(int id)
+        {
+            var dbclient = await _context.Clients.FindAsync(id);
+            if (dbclient == null)
+            {
+                return BadRequest("Client not found.");
+            }
+             else
+            {
+                _context.Clients.Remove(dbclient);
+                await _context.SaveChangesAsync();
+
+                return Ok(await _context.Clients.ToListAsync());
+            }            
+        }
+    }
+}
---
+++ WebAPI/Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime JoinedDate { get; set; }
+    }
+}
